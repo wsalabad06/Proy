075fcd86ecfbebc637651862e90a0416c5367027
wsalazarbadilla <wsalazarbadilla@gmail.com>
2024-08-08 17:18:47 -0600

    Actualiza contenido del sitio para reflejar regalos manuales

---
+++ scr/maps.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener('DOMContentLoaded', async () => {
+    await customElements.whenDefined('gmpx-store-locator');
+    const locator = document.querySelector('gmpx-store-locator');
+    locator.configureFromQuickBuilder(CONFIGURATION);
+  });
